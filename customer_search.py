170f0560c62d2d9ac7a2aa13890654808972ff3e
WeiChe-Wu <ju9818@gmail.com>
2026-04-27 13:34:33 +0800

    Update customer_search.py

--- customer_search.py
+++ customer_search.py
@@ -12,7 +12,7 @@ def get_sheet():
     creds_dict = st.secrets["gcp_service_account"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"])
     client = gspread.authorize(creds)
-    return client.open("客戶名單雲端版").sheet1
+    return client.open("customer_list_cloud").sheet1
 
 # --- 改用按需載入 ---
 @st.cache_data(ttl=600)
